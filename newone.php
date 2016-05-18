816232f60bd2cd2541e6b3bf23d76702de732a8f
PaineRiddle <paineriddle@gmail.com>
2016-05-18 21:04:03 +0800

    Update newone.php

--- newone.php
+++ newone.php
@@ -1,3 +1,11 @@
 <?php
   echo "hello";
+  $con = mysql_connect("10.10.26.58:3306","uoQqLyRr7lktUgwM","pSocJ34VnBzQOFgHW");
+  if(!$con){
+	  echo "connect Faile";	
+  }else{
+	  mysql_select_db("iwA4hU7YxaQfjb6n", $con);
+	}
+	mysql_query("UPDATE  mydb SET TRUE='1' WHERE ID=1");
+	mysql_close($con);
 ?>
