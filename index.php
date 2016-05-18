e0d36c3718bdfb6a36ed413f4ba43c097df6ce2b
PaineRiddle <paineriddle@gmail.com>
2016-05-18 22:12:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,33 @@
 define("TOKEN", "weixin");
 //$wechatObj = new wechatCallbackapiTest();
 //$wechatObj->valid();
-set_time_limit(0);
-$i=0;
-$con = mysql_connect("10.10.26.58:3306","uoQqLyRr7lktUgwM","pSocJ34VnBzQOFgHW");
-if(!$con){
-	echo "connect Faile";	
-}else{
-	mysql_select_db("iwA4hU7YxaQfjb6n", $con);
+//set_time_limit(0);
+  $con = mysql_connect("10.10.26.58:3306","uoQqLyRr7lktUgwM","pSocJ34VnBzQOFgHW");
+  if(!$con){
+	  echo "connect Faile";	
+  }else{
+	  mysql_select_db("iwA4hU7YxaQfjb6n", $con);
+	  echo "connect OK";
 	}
+	if(mysql_query("UPDATE  mydb SET TRUE=1 WHERE ID=1")) echo"UPDATE";
+	mysql_close($con);
+//$i=0;
+//$con = mysql_connect("10.10.26.58:3306","uoQqLyRr7lktUgwM","pSocJ34VnBzQOFgHW");
+//if(!$con){
+//	echo "connect Faile";	
+//}else{
+//	mysql_select_db("iwA4hU7YxaQfjb6n", $con);
+//	
+//	}
 	//mysql_close($con);
 echo "how does it work????";
-while($i<30){
-	$result = mysql_query("SELECT * FROM mydb WHERE ID=1");
-	$row = mysql_fetch_array($result);
-	if($row['TRUE']==1) echo"OK";
-	sleep(1);
-	$i++;
-}
+//while($i<30){
+//	$result = mysql_query("SELECT * FROM mydb WHERE ID=1");
+//	$row = mysql_fetch_array($result);
+//	if($row['TRUE']==1) echo"OK";
+//	sleep(1);
+//	$i++;
+//}
 
 class wechatCallbackapiTest
 {
