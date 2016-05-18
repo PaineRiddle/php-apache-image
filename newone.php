e111d7affd4512510feeb3492fed93a536349c2b
PaineRiddle <paineriddle@gmail.com>
2016-05-18 15:25:41 +0800

    Update newone.php

--- newone.php
+++ newone.php
@@ -1,5 +1,7 @@
 <?php
   echo "hello";
+  $con = mysql_connect("10.10.26.58:3306","uoQqLyRr7lktUgwM","pSocJ34VnBzQOFgHW");
+  mysql_select_db("iwA4hU7YxaQfjb6n", $con);
   $result = mysql_query("SELECT * FROM mydb WHERE ID=2");
 	while($row = mysql_fetch_array($result)){
 		echo $row['ID'] . " " . $row['NUM'];
