63336b439029b411ff661982b09f28c1c6169dc4
PaineRiddle <paineriddle@gmail.com>
2016-05-19 14:50:12 +0800

    Update conf.php

--- conf.php
+++ conf.php
@@ -5,7 +5,13 @@ $con = mysql_connect("10.10.26.58:3306","uoQqLyRr7lktUgwM","pSocJ34VnBzQOFgHW");
 if(!$con) echo "connect Faile";
 mysql_select_db("iwA4hU7YxaQfjb6n", $con);
 if(!empty($key)){
-	mysql_query("UPDATE `mydb` SET `TRUE`=1 WHERE `ID`=1");
+	$result = mysql_query("SELECT * FROM mydb WHERE ID=1");
+	$row = mysql_fetch_array($result);
+	if($row['KEY']==$key) {
+		mysql_query("UPDATE `mydb` SET `TRUE`=1 WHERE `ID`=1");
+	}else{
+		echo "wrong key";
+	} 
 }else{
 	mysql_query("UPDATE `mydb` SET `TRUE`=0 WHERE `ID`=1");
 }
