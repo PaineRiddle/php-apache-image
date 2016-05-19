5484d70bb93d77643a47e6c55df0350e0c7889c7
PaineRiddle <paineriddle@gmail.com>
2016-05-19 14:29:02 +0800

    Update gen.php

--- gen.php
+++ gen.php
@@ -1,6 +1,6 @@
 <?php
   $num = $_GET['num'];
   $key = rand(100000,999999);
-  $url = "painemriddle.daoapp.io/conf?num=".$num."&&key=".$key;
+  $url = "painemriddle.daoapp.io/con.phpf?num=".$num."&&key=".$key;
   echo $url;
 ?>
