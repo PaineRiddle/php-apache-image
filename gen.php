dbfa9be6241769895b7d5153f8cf0e2f9ccbb03b
PaineRiddle <paineriddle@gmail.com>
2016-05-20 16:22:11 +0800

    Update gen.php

--- gen.php
+++ gen.php
@@ -1,7 +1,7 @@
 <?php
   $num = $_GET['num'];
   $key = rand(100000,999999);
-  $url = "painemriddle.daoapp.io/conf.php?num=".$num."&&key=".$key."/";
+  $url = "painemriddle.daoapp.io/conf.php?num=".$num."&&key=".$key;
   $con = mysql_connect("10.10.26.58:3306","uoQqLyRr7lktUgwM","pSocJ34VnBzQOFgHW");
   if(!$con) echo "connect Faile";
   mysql_select_db("iwA4hU7YxaQfjb6n", $con);
